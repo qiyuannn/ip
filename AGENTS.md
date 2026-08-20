e37eb47034adaf64d97bc0e67591f04c43ec2c66
qiyuan <kongqiyuan31@gmail.com>
2026-08-20 17:56:48 +0800

    level-0

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: A little
+* IDE and level of expertise: A little
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Kong.java
@@ -0,0 +1,11 @@
+public class Kong {
+    public static void main(String[] args) {
+        String banner = " _  __                 \n"
+                + "| |/ /___  _ __   __ _ \n"
+                + "| ' // _ \\| '_ \\ / _` |\n"
+                + "| . \\ (_) | | | | (_| |\n"
+                + "|_|\\_\\___/|_| |_|\\__, |\n"
+                + "                 |___/ \n";
+        System.out.println(banner);
+    }
+}
