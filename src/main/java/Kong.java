81cdc0194b7196bc7c4f65fa207c8eaa2027d53c
qiyuan <kongqiyuan31@gmail.com>
2026-08-20 19:00:58 +0800

    Level 2

--- src/main/java/Kong.java
+++ src/main/java/Kong.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Kong {
     public static void main(String[] args) {
@@ -9,19 +10,37 @@ public class Kong {
                 + "|_|\\_\\___/|_| |_|\\__, |\n"
                 + "                 |___/ \n";
 
-        String line = "________________________________________\n";
-        String out = banner + line + "Hello, I'm Kong.\nWhat can I do for you?\n" + line;
+        String line = "________________________________________";
+        String out = banner + line + "\n" + "Hello, I'm Kong.\nWhat can I do for you?\n" + line;
         System.out.println(out);
+
+        ArrayList<String> lst =  new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(line + "Bye. Hope to see you again soon!");
-                break;
+            System.out.println(line);
+
+            switch (input) {
+                case "":
+                    System.out.println("Please enter a valid command.");
+                    System.out.println(line);
+                    continue;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    scanner.close();
+                    return;
+                case "list":
+                    for (int i = 0; i < lst.size(); i++) {
+                        System.out.println(String.format("%d. %s", i + 1, lst.get(i)));
+                    }
+                    System.out.println(line);
+                    continue;
+                default:
+                    lst.add(input);
+                    System.out.println("added: " + input);
+                    System.out.println(line);
             }
-            System.out.println(line + input + "\n" +line);
         }
-        scanner.close();
     }
 }
